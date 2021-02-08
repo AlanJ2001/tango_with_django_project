fefd47232bc1a711f4d3b80f9eb3673036f2238d
Alan Joy <2460256J@student.gla.ac.uk>
2021-02-08 20:45:40 +0000

    Chapter 9 and exercises completed

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -11,6 +14,7 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ('name',)
+
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
@@ -32,3 +36,20 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
+
+
+
